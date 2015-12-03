80743e69eeeb95bd32dfaca7f1413108a29ee315
alex <alexandre.louys@univ-lyon1.fr>
2015-12-03 19:19:36 +0100

    Earth and seism with slider !!! Not Finished

---
+++ 3DGeoSciences/Assets/Scripts/RaycastSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastSpawner : MonoBehaviour {
+
+
+	public GameObject prefab;
+
+	private float lastTimeMouseButtonDown;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			lastTimeMouseButtonDown = Time.time;
+		}
+
+		if(Input.GetMouseButtonUp(0) && Time.time - lastTimeMouseButtonDown < 0.1f){
+			SpawnOnHit ();
+		}
+
+	}
+
+	void SpawnOnHit(){
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit) == true)
+		{
+			GameObject go = (GameObject) Instantiate(prefab, hit.point, Quaternion.FromToRotation(Vector3.up , hit.normal));
+			// go.transform.Rotate(Vector3.right * 90f);
+			go.transform.SetParent(transform);
+		}
+	}
+}
